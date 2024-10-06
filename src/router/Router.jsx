8609ea36e5d51b590f7876a5cac25dc6bfa5a535
Christian <filetelopezchristian@gmail.com>
2024-10-06 13:10:44 -0600

    Non-important changes :monkey:

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,12 +3,20 @@ import { Navigate } from 'react-router-dom'
 import MainLayout from '../layout/MainLayout'
 import Notes from '../pages/notes/Notes'
 import Login from '../pages/Login'
+import ProtectedRoute from './ProtectedRoute'
 
 const Router = () => {
 	return (
 		<Routes>
 			<Route path='/' element={<Navigate to='/notes' />} />
-			<Route path='/notes' element={<MainLayout />}>
+			<Route
+				path='/notes'
+				element={
+					<ProtectedRoute>
+						<MainLayout />
+					</ProtectedRoute>
+				}
+			>
 				<Route index element={<Notes />} />
 			</Route>
 			<Route path='/login' element={<MainLayout />}>
