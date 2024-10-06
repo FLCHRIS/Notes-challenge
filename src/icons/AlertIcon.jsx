c19013cb257462229f8989d9756e8d482e4c0c9a
Christian <filetelopezchristian@gmail.com>
2024-10-06 13:08:03 -0600

    New icon added

---
+++ src/icons/AlertIcon.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const AlertIcon = ({ style }) => {
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			width='24'
+			height='24'
+			viewBox='0 0 24 24'
+			fill='none'
+			stroke='currentColor'
+			strokeWidth='1.5'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			className={style}
+		>
+			<title>Alert</title>
+			<path stroke='none' d='M0 0h24v24H0z' fill='none' />
+			<path d='M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0' />
+			<path d='M12 9v4' />
+			<path d='M12 16v.01' />
+		</svg>
+	)
+}
+
+export default AlertIcon
