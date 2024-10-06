cc621b6f627fa99f6a0b23d7b682faa191a1c6f9
Christian <filetelopezchristian@gmail.com>
2024-10-06 13:10:02 -0600

    New background added

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -5,9 +5,12 @@ const MainLayout = () => {
 	return (
 		<>
 			<NavBar />
-			<main className='bg-white dark:bg-gray-900 px-6 py-5 w-full max-w-[1440px] mx-auto'>
+			<main className='px-6 py-5 w-full max-w-[1440px] mx-auto'>
 				<Outlet />
 			</main>
+			<div className='fixed -z-10 top-0 left-0 h-full w-full bg-white dark:bg-gray-900'>
+				<div className='absolute h-full w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] dark:bg-[radial-gradient(#374151a7_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]' />
+			</div>
 		</>
 	)
 }
