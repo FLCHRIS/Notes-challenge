8f72cc117cd480f1f85063c67647a09ae3526845
Christian <filetelopezchristian@gmail.com>
2024-10-06 13:08:23 -0600

    New file created

---
+++ src/router/ProtectedRoute.jsx
@@ -0,0 +1,20 @@
+import { useContext, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { getLocalStorage } from '../config/localStorage'
+import UserContext from '../context/user/UserContext'
+
+const ProtectedRoute = ({ children }) => {
+	const { user, logIn } = useContext(UserContext)
+	const navigate = useNavigate()
+
+	useEffect(() => {
+		const session = getLocalStorage('session')
+		if (!user && session) return logIn(session)
+		if (!user && !session) return navigate('/login')
+		console.log('hola')
+	}, [user, logIn, navigate])
+
+	return <>{children}</>
+}
+
+export default ProtectedRoute
