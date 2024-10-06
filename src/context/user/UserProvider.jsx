6fbda48bd1db0d03230b353ed3381ff770b0597d
Christian <filetelopezchristian@gmail.com>
2024-10-06 13:09:26 -0600

    Non-important changes :monkey:

--- src/context/user/UserProvider.jsx
+++ src/context/user/UserProvider.jsx
@@ -1,19 +1,15 @@
 import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
 import UserContext from './UserContext'
 
 const UserProvider = ({ children }) => {
-	const navigate = useNavigate()
 	const [user, setUser] = useState(null)
 
 	const logIn = (user) => {
 		setUser(user)
-		navigate('/notes')
 	}
 
 	const logOut = () => {
 		setUser(null)
-		navigate('/login')
 	}
 
 	return (
