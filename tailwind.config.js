1c259432034c2f7e67ec3c0766c9a5a60e36c432
Christian <filetelopezchristian@gmail.com>
2024-10-06 13:11:05 -0600

    Non-important changes :monkey:

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,16 @@ export default {
     extend: {
       fontFamily: {
         sans: ['Noto Sans Variable', ...defaultTheme.fontFamily.sans],
-      }
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 0.5s ease-in-out',
+      },
     },
   },
   plugins: [],
