915085fc6c2409fd43246ddb2320a0cf4dca6b1f
Christian <filetelopezchristian@gmail.com>
2024-10-06 13:09:43 -0600

    Non-important changes :monkey:

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,7 +1,26 @@
 import { useState } from 'react'
 
-const useForm = () => {
-	return {}
+const useForm = (initialValues, initialErrors) => {
+	const [values, setValues] = useState(initialValues)
+	const [error, setError] = useState(initialErrors)
+
+	const handleChange = (e) => {
+		const { name, value } = e.target
+		setValues({ ...values, [name]: value })
+	}
+
+	const handleSubmit = (e, callback) => {
+		e.preventDefault()
+		callback()
+	}
+
+	return {
+		values,
+		error,
+		handleChange,
+		handleSubmit,
+		setError,
+	}
 }
 
 export default useForm
