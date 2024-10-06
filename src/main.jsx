9d6eea920e62eceddb2dc508ed21792d7254826f
Christian <filetelopezchristian@gmail.com>
2024-10-06 13:10:16 -0600

    Non-important changes :monkey:

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-// Supports weights 100-900
 import '@fontsource-variable/noto-sans'
 
 import App from './App.jsx'
