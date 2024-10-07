732934a73a5c0758c80c106c2b74aaaffa879d08
Christian <filetelopezchristian@gmail.com>
2024-10-06 18:04:14 -0600

    Non-important changes :monkey:

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -8,14 +8,16 @@ const Input = ({ children, id, label, placeholder, type, onChange, value }) => {
 				{label}
 			</label>
 			<div className='relative'>
-				<div className='absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none'>
-					{children}
-				</div>
+				{children && (
+					<div className='absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none'>
+						{children}
+					</div>
+				)}
 				<input
 					type={type}
 					id={id}
 					name={id}
-					className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+					className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ${children && 'ps-10'}`}
 					placeholder={placeholder}
 					onChange={onChange}
 					value={value}
