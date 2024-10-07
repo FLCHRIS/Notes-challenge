b7a67912040aab7482508412c25e69085aa7fff1
Christian <filetelopezchristian@gmail.com>
2024-10-07 10:45:23 -0600

    Non-important changes :monkey:

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,9 +17,14 @@ export default {
           '0%': { opacity: 0 },
           '100%': { opacity: 1 },
         },
+        translateIn: {
+          '0%': { opacity: 0, transform: 'translateY(-100%)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
       },
       animation: {
         fadeIn: 'fadeIn 0.5s ease-in-out',
+        translateIn: 'translateIn 0.5s ease-in-out',
       },
     },
   },
