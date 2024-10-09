45292be5ba8ec886d9e2157a9eb0f239e81c9b76
Christian <filetelopezchristian@gmail.com>
2024-10-09 10:22:49 -0600

    Finished file

--- src/components/MultiSelect.jsx
+++ src/components/MultiSelect.jsx
@@ -30,7 +30,7 @@ const MultiSelect = ({ label, values, setValues }) => {
 			</p>
 			<button
 				type='button'
-				className={`bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 flex items-center justify-between ${showDropdown && 'scale-95 opacity-80'} transition-transform`}
+				className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white flex items-center justify-between ${showDropdown && 'scale-95 opacity-80'} transition-transform`}
 				onClick={() => setShowDropdown(!showDropdown)}
 			>
 				<span
@@ -43,7 +43,7 @@ const MultiSelect = ({ label, values, setValues }) => {
 				<ChevronIcon style='w-5 h-5 text-gray-800 dark:text-gray-300' />
 			</button>
 			{showDropdown && (
-				<div className='absolute animate-translateIn z-0 left-0 top-[calc(100%+8px)] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full dark:bg-gray-700 dark:border-gray-600 dark:text-white max-h-36 overflow-y-auto'>
+				<div className='absolute animate-fadeInDown z-0 left-0 top-[calc(100%+8px)] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full dark:bg-gray-700 dark:border-gray-600 dark:text-white max-h-36 overflow-y-auto'>
 					<ul className='flex flex-col gap-1 overflow-hidden'>
 						<li>
 							<button
