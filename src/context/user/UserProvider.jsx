dd752a648c0a0bebde0fac5830440cf51a19f404
Christian <filetelopezchristian@gmail.com>
2024-10-11 08:56:41 -0600

    Non-important changes :monkey:

--- src/context/user/UserProvider.jsx
+++ src/context/user/UserProvider.jsx
@@ -1,19 +1,30 @@
 import { useState } from 'react'
 import UserContext from './UserContext'
+import { setLocalStorage, removeLocalStorage } from '../../config/localStorage'
 
 const UserProvider = ({ children }) => {
 	const [user, setUser] = useState(null)
 
 	const logIn = (user) => {
-		setUser(user)
+		setUser(() => {
+			setLocalStorage('session', user)
+			return user
+		})
 	}
 
 	const logOut = () => {
-		setUser(null)
+		setUser(() => {
+			removeLocalStorage('session')
+			return null
+		})
+	}
+
+	const createSession = (user) => {
+		setUser(user)
 	}
 
 	return (
-		<UserContext.Provider value={{ user, logIn, logOut }}>
+		<UserContext.Provider value={{ user, logIn, logOut, createSession }}>
 			{children}
 		</UserContext.Provider>
 	)
