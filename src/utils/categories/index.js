c72f8c983bfd45aa29ca9844bf621a98c5a5ab3e
Christian <filetelopezchristian@gmail.com>
2024-10-12 09:36:05 -0600

    Created file

---
+++ src/utils/categories/index.js
@@ -0,0 +1,10 @@
+export const CATEGORIES = [
+	{ id: 1, name: 'Work' },
+	{ id: 2, name: 'Personal' },
+	{ id: 3, name: 'Study' },
+	{ id: 4, name: 'Projects' },
+	{ id: 5, name: 'Ideas' },
+	{ id: 6, name: 'Shopping' },
+	{ id: 7, name: 'To-Do' },
+	{ id: 8, name: 'Health' },
+]
